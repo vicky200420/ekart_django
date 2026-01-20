947391d2f122556c38c30a4a834e41713093ca48
vicky200420 <vikkydhana08329@gmail.com>
2026-01-20 11:25:21 +0530

    Fixed ALLOWED_HOSTS for production

--- ecommerces/settings.py
+++ ecommerces/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!-29fx(dzciedwwopqx@i*uy(ac5*d=dj^gt1#omy34)lgkmsz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
