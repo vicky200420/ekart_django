9f5cc7d39679d8b9a551f75c41fd1439b83d9164
vicky200420 <vikkydhana08329@gmail.com>
2026-01-21 12:38:52 +0530

    changes

--- ecommerces/settings.py
+++ ecommerces/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!-29fx(dzciedwwopqx@i*uy(ac5*d=dj^gt1#omy34)lgkmsz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
