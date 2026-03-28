3961ccd906faec1e11f9c6cc9cdbf69cb6348b96
vicky200420 <vikkydhana08329@gmail.com>
2026-03-29 00:00:21 +0530

    changes design

--- ecommerces/settings.py
+++ ecommerces/settings.py
@@ -85,6 +85,12 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
--- shop/urls.py
+++ shop/urls.py
@@ -4,9 +4,9 @@ from .views import*
 
 urlpatterns = [
     path('',home,name="home"),
-    path('register',register,name="register"),
-    path('login',login_page,name="login"),
-    path('logout',logout_page,name="logout"),
+    path('register/',register,name="register"),
+    path('login/',login_page,name="login"),
+    path('logout/',logout_page,name="logout"),
     path('cart',cart_pages,name='cart'),
     path('delete/<str:cid>',remove_cart,name='delete'),
     path('fav',fav_pages,name='fav'),
